ec42519cf0d828abbccfc2c816ed6542a2c78490
Hana <hanadelcourt@gmail.com>
2025-04-05 15:54:20 +0200

    comment english button listener

--- script.js
+++ script.js
@@ -219,7 +219,7 @@ async function displayEducation(langue = "fr") {
 language = "fr";
 
 document.querySelector("#btn-lang-fr").addEventListener("click", () => setLangInURL("fr"));
-document.querySelector("#btn-lang-en").addEventListener("click", () => setLangInURL("en"));
+//document.querySelector("#btn-lang-en").addEventListener("click", () => setLangInURL("en"));
 
 // Appeler la fonction pour afficher le parcours
 displayEducation();
